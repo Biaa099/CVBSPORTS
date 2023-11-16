af71ad53fd71604593bc5e0bb6e641406f2209df
Beatriz Ferreira <133707298+Biaa099@users.noreply.github.com>
2023-11-16 18:09:24 -0300

    Delete src directory

--- src/componentes/Carta/index.js
+++
@@ -1,9 +0,0 @@
-import newsletter from '../../imagens/newsletter.svg'
-import './style.css'
-
-function Carta(){
-    return(
-        <img src={newsletter} id="carta" alt="Carta" />
-    )
-}
-export default Carta
